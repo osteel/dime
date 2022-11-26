6ddf1fed22fdce78b0e09506a31b46f0039c3c3a
osteel <yannickchenot@gmail.com>
2022-11-26 13:34:30 +0000

    untracked database file and added Tinker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,19 +5,10 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Intonate\TinkerZero\TinkerZeroServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
     /**
      * Register any application services.
      *
@@ -25,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(TinkerZeroServiceProvider::class);
+        }
     }
 }
