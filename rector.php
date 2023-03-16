0b7fb2f103df5534162a31c82cbd784874165aab
Yannick Chenot <yannick@yellowraincoat.co.uk>
2023-03-16 11:34:32 +0000

    added type coverage to PHPStan (#24)

--- rector.php
+++ rector.php
@@ -8,7 +8,7 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/app',
-        __DIR__ . '/domain',
+        __DIR__ . '/domain/src',
     ]);
 
     $rectorConfig->sets([LevelSetList::UP_TO_PHP_82]);
