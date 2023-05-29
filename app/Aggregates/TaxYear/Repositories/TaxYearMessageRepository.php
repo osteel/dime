8bb8df113fe1c19934e8182fcdbcdc17489bd37e
osteel <yannickchenot@gmail.com>
2023-05-29 12:06:48 +0100

    100% test coverage in app folder

--- app/Aggregates/TaxYear/Repositories/TaxYearMessageRepository.php
+++ app/Aggregates/TaxYear/Repositories/TaxYearMessageRepository.php
@@ -5,14 +5,8 @@ declare(strict_types=1);
 namespace App\Aggregates\TaxYear\Repositories;
 
 use Domain\Aggregates\TaxYear\Repositories\TaxYearMessageRepository as TaxYearMessageRepositoryInterface;
-use Domain\Aggregates\TaxYear\ValueObjects\TaxYearId;
 use EventSauce\MessageRepository\IlluminateMessageRepository\IlluminateUuidV4MessageRepository;
-use Generator;
 
 final class TaxYearMessageRepository extends IlluminateUuidV4MessageRepository implements TaxYearMessageRepositoryInterface
 {
-    public function all(TaxYearId $taxYearId): Generator
-    {
-        return $this->retrieveAll($taxYearId);
-    }
 }
--- domain/src/Aggregates/TaxYear/Repositories/TaxYearMessageRepository.php
+++ domain/src/Aggregates/TaxYear/Repositories/TaxYearMessageRepository.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Domain\Aggregates\TaxYear\Repositories;
 
-use Domain\Aggregates\TaxYear\ValueObjects\TaxYearId;
-use Generator;
-
 interface TaxYearMessageRepository
 {
-    public function all(TaxYearId $taxYearId): Generator;
 }
