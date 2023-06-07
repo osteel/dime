323fbca05a9fba73dbda62a738f4e34194278b82
osteel <yannickchenot@gmail.com>
2023-06-07 16:38:44 +0100

    updated build

--- app/Services/SelfUpdate/Helpers.php
+++ app/Services/SelfUpdate/Helpers.php
@@ -6,6 +6,7 @@ final class Helpers
 {
     public static function installedViaComposer(): bool
     {
+        dump(__DIR__);
         return str_contains(__DIR__, '.composer/vendor/bin');
     }
 }
